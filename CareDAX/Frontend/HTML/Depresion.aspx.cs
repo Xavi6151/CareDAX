9da474213e427d1b2731c78cf652d41d14c22ad6
Xavi6151 <s20120194@alumnos.itsur.edu.mx>
2024-03-25 22:27:27 -0600

    Se hizo todo de la parte de estres (sin contar el diseño), se hizo conexion a la pagina Principal.aspx cuando se le de al boton de Regresar, se calculan los niveles de estres y se bloquea la tabla para evitar una puntuación engañosa confiando unicamente en que el usuario conteste sinceramente, estos datos se le dan en la descripción que esta al inicio de la pagina.

--- CareDAX/Frontend/HTML/Depresion.aspx.cs
+++ CareDAX/Frontend/HTML/Depresion.aspx.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using CareDAX.Backend.Strings;
 
 namespace CareDAX.Frontend.HTML
 {
-    public partial class DAXIA : System.Web.UI.Page
+    public partial class Depresion : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            nombre.Text = StringsDaxia.nombreIA;
-            Infomessage.Text = StringsDaxia.Infomessage;
-            request.Text = StringsDaxia.request;
+
         }
     }
 }
